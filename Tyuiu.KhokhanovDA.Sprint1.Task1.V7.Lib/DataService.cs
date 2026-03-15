b2c094e866fc7b70ed2870de8bef46766ff46393
Хоханов Данил111 <ebefur6@gmail.com>
2026-03-15 22:47:41 +0500

    Sprint 1 Task 1

---
+++ Tyuiu.KhokhanovDA.Sprint1.Task1.V7.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.KhokhanovDA.Sprint1.Task1.V7.Lib
+{
+    public class DataService : ISprint1Task1V7
+    {
+        public double Calculate(double x, double y)
+        {
+            return (5 * x) / (y + x);
+        }
+    }
+}
