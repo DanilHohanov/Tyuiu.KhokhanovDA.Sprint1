b4919d5732d81330b2d8a8348a9f47b77f186302
Хоханов Данил111 <ebefur6@gmail.com>
2026-03-15 21:07:32 +0500

    Add project files.

---
+++ Tyuiu.KhokhanovDA.Sprint1.Task0.V26/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.KhokhanovDA.Sprint1.Task0.V26.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Sprint #1 | Made by Khokhanov D.A. | НТм-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #26                                                             *");
+        Console.WriteLine("* Выполнил: Хоханов Данил Анатольевич | НТм-24-1                          *");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать консольную программу, которая вычисляет выражение 4*5/2-18/2/3 *");
+        Console.WriteLine("* и печатает результат на экране                                          *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* 4 * 5 / 2 - 18 / 2 / 3                                                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.Calculate());
+        Console.ReadLine();
+    }
+}
